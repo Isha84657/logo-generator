0714cb77c1dfb035c5229cca02ed122a7ff54ffd
Satyam Mishra <satyammishra9050@gmail.com>
2024-10-29 23:17:04 +0530

    added multiple text adding, text size changing, font changing, text deleting, save as png, save as jpg

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ImagePrompt from './components/ImagePrompt';
-import ImageEditor from './components/ImageEditor';
+import EditLogo from './components/EditLogo';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ImagePrompt />} />
-        <Route path="/edit_logo" element={<ImageEditor />} />
+        <Route path="/edit_logo" element={<EditLogo />} />
       </Routes>
     </Router>
   );
